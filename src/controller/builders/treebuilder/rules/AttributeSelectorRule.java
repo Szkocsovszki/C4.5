a0f7cace364d0e749a031b6f8ee3729542c80304
Szkocsovszki Zsolt <szkocsovszki.zsolt@gmail.com>
2019-05-04 20:05:16 +0200

    refactoring

--- src/controller/builders/treebuilder/rules/AttributeSelectorRule.java
+++ src/controller/builders/treebuilder/rules/AttributeSelectorRule.java
@@ -10,13 +10,13 @@ import controller.operations.VectorOperations;
 
 public class AttributeSelectorRule {
 	
-	private static double entropy(double w_plus, double xki_minus, double w_minus, double xki_plus) {
+	private static double vagueness(double w_plus, double xki_minus, double w_minus, double xki_plus) {
 		//return 1 / (1 + w_plus*((1-xki_minus)/xki_minus) + w_minus*((1-xki_plus)/xki_plus));
 		return (xki_plus*xki_minus) / (w_plus*xki_plus + w_minus*xki_minus);
 	}
 	
-	private static void createEntropyList(
-			ArrayList<Double> entropyList, 
+	private static void createVaguenessList(
+			ArrayList<Double> vaguenessList, 
 			ColumnVector classVectorPositive, ColumnVector classVectorNegative, 
 			ArrayList<ColumnVector> r_plus, ArrayList<ColumnVector> r_minus
 	) {
@@ -24,19 +24,19 @@ public class AttributeSelectorRule {
 		double w_plus = (double) classVectorPositive.getSum() / size;
 		double w_minus = (double) classVectorNegative.getSum() / size;
 		double xki_plus, xki_minus;
-		double attributeEntropy;
+		double attributeVagueness;
 		String name;
 		for(String attributeName : CaseInformation.attributeNames) {
-			attributeEntropy = 0;
+			attributeVagueness = 0;
 			for(String value : CuttingInformation.possibleValuesForAttribute(attributeName)) {
 				name = VectorInformation.getVectorFromVectorListByName(value).getName();
 				xki_plus = VectorInformation.getVectorFromVectorListByName(CaseInformation.positiveClassification + name, r_plus).getSum() / 
 						   classVectorPositive.getSum();
 				xki_minus = VectorInformation.getVectorFromVectorListByName(CaseInformation.negativeClassification + name, r_minus).getSum() / 
 						   classVectorNegative.getSum();
-				attributeEntropy += entropy(w_plus, xki_minus, w_minus, xki_plus);
+				attributeVagueness += vagueness(w_plus, xki_minus, w_minus, xki_plus);
 			}
-			entropyList.add(attributeEntropy);
+			vaguenessList.add(attributeVagueness);
 		}
 	}
 	
@@ -49,12 +49,12 @@ public class AttributeSelectorRule {
 		
 		VectorOperations.scalarMultiplicationWithClassVectors(classVectorPositive, classVectorNegative, r_plus, r_minus);
 		
-		ArrayList<Double> entropyList = new ArrayList<>();
-		createEntropyList(entropyList, classVectorPositive, classVectorNegative, r_plus, r_minus);
+		ArrayList<Double> vaguenessList = new ArrayList<>();
+		createVaguenessList(vaguenessList, classVectorPositive, classVectorNegative, r_plus, r_minus);
 		
-		double min = entropyList.get(0);
+		double min = vaguenessList.get(0);
 		int counter = 0, index = 0;
-		for(double i : entropyList) {
+		for(double i : vaguenessList) {
 			if(i < min) {
 				min = i;
 				index = counter;
